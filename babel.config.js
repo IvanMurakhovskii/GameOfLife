6127d3f6a5edb0e6201aa493f70933e158a7aa14
ia.murakhovskii <ia.murakhovskii@stalcom.com>
2020-11-16 12:40:41 +0300

    feat: configurate github pages

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,5 @@
 module.exports = {
   presets: ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"],
   plugins: ["@babel/plugin-proposal-class-properties", "@babel/transform-runtime"],
-  env: {
-    production: {
-      plugins: ["emotion"],
-    },
-    development: {
-      plugins: [["emotion", { sourceMap: true }]],
-    }
-  }
+  
 };
